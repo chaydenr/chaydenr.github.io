69eca87ae3c36da22dca57f8877543239700c166
chaydenr <75277857+chaydenr@users.noreply.github.com>
2026-06-05 23:58:25 -1000

    Configure setup for SVG and Tailwind CSS handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg',
+      svgrOptions: {
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
