af26a19e2a0b453e246727508c2a526c7c792a04
chaydenr <75277857+chaydenr@users.noreply.github.com>
2026-06-07 19:14:05 -1000

    Create Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {Header} from './sections/Header.tsx'
+import { HeroSection } from './sections/Hero.tsx'
 
 function App() {
 
   return (
     <div className="bg-gray-900 text-white antialiased font-sans min-h-screen">
       <Header />
+      <HeroSection />
     </div>
   )
 }
